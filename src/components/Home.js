4b1ce22209c4f0ac3f9b63c0fb04e9be59d30c0c
garrengotthardt <garren.x.gotthardt@gmail.com>
2017-11-10 10:54:44 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
     <Container>
       <img id='homeMap' src='/home_map_rectangle.png'/>
       <h1 id='homeHeader'>Welcome to Break5</h1>
-      <p id='homeBody'>Looking for a quick bite on a budget? Break5 helps you find establisments near you with options under $5</p>
+      <p id='homeBody'>Looking for a quick bite on a budget? Break5 helps you find establishments near you with options under $5</p>
 
       <div id='loginSignupButtons'>
         <Link to='/login'><Button className='homeButton' basic color='black' type='submit'>Log In</Button></Link>
